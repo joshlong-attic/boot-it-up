47aca700e785c9736ef462b1a1e56714b66ca749
Josh Long <josh@joshlong.com>
2014-02-18 21:43:40 -0800

    removing target

---
+++ code/testing/src/main/java/demo/Application.java
@@ -0,0 +1,91 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
+        ReservationRepository reservationRepository = applicationContext.getBean(ReservationRepository.class);
+
+        for (Reservation r : reservationRepository.findAll())
+            System.out.println(r.toString());
+    }
+}
+
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
+
+@Entity
+class Reservation {
+
+    @Column(name = "group_size")
+    private int groupSize = 1;
+
+    @Column(name = "date_and_time")
+    private Date dateAndTime;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "groupSize=" + groupSize +
+                ", dateAndTime=" + dateAndTime +
+                ", id=" + id +
+                ", familyName='" + familyName + '\'' +
+                '}';
+    }
+
+    @Column(name = "family_name")
+    private String familyName;
+
+    public int getGroupSize() {
+        return groupSize;
+    }
+
+    public void setGroupSize(int groupSize) {
+        this.groupSize = groupSize;
+    }
+
+    public Date getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(Date dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+}
